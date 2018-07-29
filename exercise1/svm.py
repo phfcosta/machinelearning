f549407d9e76bbeddf05a4e083547ee70903fd62
Paulo Costa <paulohfcosta@outlook.com>
2018-07-29 16:57:47 -0700

    update svm project

--- exercise1/svm.py
+++ exercise1/svm.py
@@ -1,5 +1,5 @@
 import sys
-from class_vis import pretty_picture
+from class_vis import prettyPicture
 from prep_terrain_data import makeTerrainData
 
 import matplotlib.pyplot as plt
@@ -7,15 +7,17 @@ import copy
 import numpy as np
 import pylab as pl
 
-features_train, label_train, features_test, labels_test = makeTerrainData()
+features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 from sklearn.svm import SVC
 clf = SVC(kernel="linear")
-
-
+clf.fit(features_train,labels_train)
+pred = clf.predict(features_test)
 
 from sklearn.metrics import accuracy_score
 acc = accuracy_score(pred,labels_test)
 
 def submitAccuracy():
 	return acc
+
+print(str(acc))
